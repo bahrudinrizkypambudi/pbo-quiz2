68fc74dcd79096785d63cc2ebc5016610255939d
Sintongs <Sintongs@DESKTOP-N0EMNOQ>
2018-11-30 00:34:21 +0700

    penambahan class comboboxmodel

--- test/ComboBoxModel.java
+++ test/ComboBoxModel.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /*
@@ -15,5 +16,28 @@ public class ComboBoxModel {
     private HashMap<String,Float> items = new HashMap<>();
     
     public ComboBoxModel(){
+        this.items.put("Gula", new Float(30000));
+        this.items.put("Kopi", new Float(10000));
+        this.items.put("Susu", new Float(20000));
+    }
+    
+    public ArrayList<String> getNames(){
+        ArrayList<String> str = new ArrayList<>();
+        for(String item : this.items.keySet()){
+            str.add(item);
+        }
+        return str;
+    }
+    
+        public ArrayList<Float> getPrices() {
+        ArrayList<Float> flt = new ArrayList<>();
+        for(float item : this.items.values()) {
+            flt.add(item);
+        }
+        return flt;
+    }
+        public void addItem(String name, float price){
+            this.items.put(name, price);
+       
     }
 }
